7e43248efa414929f78c79414a425e10b5ba2314
Rogier <rogier02@gmail.com>
2022-11-28 23:43:43 +0100

    Started creating the basic layout of the (random) word choosing infrastructure (with included text file; need to think about if this list is good enough for word generation and word checking) and made an awesome text based logo displayed when fired up.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,78 @@
 #include <iostream>
-#include "math.h"
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-escape-sequence"
 using namespace std;
+
+string wordChoosingFunction() {
+    fstream fin;
+
+    //import the csv file containing the words
+    fin.open("valid-words.csv", ios::in);
+
+    //ask for specific word in index
+    int wordNumber;
+    cout << "Index number of the word? \n";
+    cin >> wordNumber;
+
+    //TODO add the choosing of the word in the wordslist
+    return "ready";
+
+};
+
+void inputFunction() {
+
+}
+
+void checkingInputFunction() {
+
+}
+
+void correctingFunction() {
+
+}
+
+void outputFunction() {
+
+}
+
+void countingFunction() {
+
+}
+
+void revealWordFunction() {
+
+}
+
+void fontWordle() {
+
+    system("clear"); //*nix
+
+    cout << "    __          __           _ _       \n";
+    cout << "    \\ \\        / /          | | |      \n";
+    cout << "     \\ \\  /\\  / /__  _ __ __| | | ___  \n";
+    cout << "      \\ \\/  \\/ / _ \\| '__/ _` | |/ _ \\ \n";
+    cout << "       \\  /\\  / (_) | | | (_| | |  __/ \n";
+    cout << "        \\/  \\/ \\___/|_|  \\__,_|_|\\___| \n";
+    cout << "\n";
+    cout << "A wordle game in C++, created by Trui en Frans 2022 \n"; //TODO fix names
+
+
+}
 
 int main() {
     int i =3;
-    cout << "Hello, Wordle, ik ben Trui!\n";
+    cout << "Hello, Wordle \n";
+    fontWordle();
+    string finalWord = wordChoosingFunction();
+    cout << "Final word is: " << finalWord << " \n"; //TODO remove when finished
+
+
     return 0;
 }
+
+#pragma clang diagnostic pop
