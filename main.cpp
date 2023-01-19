b944a0448a348fb70df7b89caf53649eb86d506d
Jochem Schuerman <jochemschuerman@kpnmail.nl>
2023-01-19 16:10:21 +0100

    Laatste commit

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main() {
     Game game;                              //create an instance of the Game class
     game.fontWordle();                      //prints the welcome screen
     while (true) {                          //while the users wants to keep playing
-        game.play();                        //run the play command
+        game.play(developMode);             //run the play command
         string userInput = "?";
         cout << "Do you want to play another game?\n";                  //ask the user if he wants to play again
         while (userInput != "y" && userInput != "n") {                  //if the user does not fill in y or n
