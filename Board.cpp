69a680f185f1f66368701dbadebaba8b22a65836
Jochem Schuerman <jochemschuerman@kpnmail.nl>
2023-01-19 14:38:44 +0100

    New game feature

--- Board.cpp
+++ Board.cpp
@@ -11,7 +11,7 @@ Board::Board() {                                        //gives dots when nothin
     fill_n (corrections, 6*5, 0);
 }
 
-string toFullColor(char colorChar) {                    //converts 
+string toFullColor(char colorChar) {                    //converts r/g/y into strings 
     if (colorChar == 'y') {
         return "yellow";
     }
